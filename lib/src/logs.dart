ec53d2c91c48e34c6bad57bbc439ff6dd48a7f3b
iLnaar <ilnv@mail.ru>
2021-07-26 14:52:03 +0300

    заработало

--- lib/src/logs.dart
+++ lib/src/logs.dart
@@ -1,4 +1,73 @@
 
-void log(dynamic data) {
-  print(data);
+
+import 'dart:io';
+
+
+class Log {
+  var archiveFolder;
+  var fileName;
+  var maxLogFileSize;
+  var archiveFileTimeLength;
+  void Function(dynamic error)? onError;
+  final File _file;
+  late final RandomAccessFile _writer;
+
+
+  Log({
+    this.fileName = 'log.txt',
+    this.archiveFolder = 'log_archive\\',
+    this.maxLogFileSize = 8388608,
+    this.archiveFileTimeLength = 21,
+    this.onError
+  }): _file = File(fileName) {
+    try {
+      _writer = _file.openSync(mode: FileMode.writeOnlyAppend);
+    } catch(error) {
+      if (onError == null) {
+        Exception(error);
+      } else {
+        onError?.call(error);
+      }
+    }
+  }
+
+
+  void log(dynamic data, {
+    bool showTime = true,
+    int timeLength = 23,
+    String afterTime = '  ',
+    bool toConsole = true,
+    bool toFile = true
+  }) {
+    try {
+      var s = '';
+      if (showTime) {
+        s = timeString(timeLength) + afterTime;
+      }
+      s += data.toString();
+      if (toConsole) print(s);
+      if (toFile) _writer.writeStringSync(s);
+      _logToArchive();
+    } catch(error) {
+      if (onError == null) {
+        throw Exception(error);
+      } else {
+        onError?.call(error);
+      }
+    }
+  }
+
+
+  void _logToArchive() {
+    if (_file.lengthSync() >= maxLogFileSize) {
+      var time = timeString(archiveFileTimeLength);
+      _file.copySync(archiveFolder + time + ' - ' + fileName);
+      _writer.setPositionSync(0);
+      _writer.truncateSync(0);
+    }
+  }
+
+
+  String timeString(int length) =>
+    DateTime.now().toString().substring(0, length).replaceAll(':', '.');
 }
