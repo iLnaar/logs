330b78b009f7490f2648814d552e3d00ec4e83d6
iLnaar <ilnv@mail.ru>
2021-09-08 19:48:50 +0300

    добавлен буфер и параметр immediatelyToFile в log()

--- lib/src/logs.dart
+++ lib/src/logs.dart
@@ -4,10 +4,13 @@ import 'dart:io';
 
 
 class Log {
-  var archiveFolder;
-  var fileName;
-  var maxLogFileSize;
+  String archiveFolder;
+  String fileName;
+  int maxLogFileSize;
+  int maxBufferSize;
   var archiveFileTimeLength;
+  late int _fileSize;
+  var _buffer = '';
   void Function(dynamic error)? onError;
   final File _file;
   late final RandomAccessFile _writer;
@@ -17,11 +20,13 @@ class Log {
     this.fileName = 'program.log',
     this.archiveFolder = 'log_archive\\',
     this.maxLogFileSize = 8388608,
+    this.maxBufferSize = 32768,
     this.archiveFileTimeLength = 21,
     this.onError
   }): _file = File(fileName) {
     try {
       _writer = _file.openSync(mode: FileMode.writeOnlyAppend);
+      _fileSize = _file.lengthSync();
     } catch(error) {
       if (onError == null) {
         Exception(error);
@@ -37,7 +42,8 @@ class Log {
     int timeLength = 23,
     String afterTime = '  ',
     bool toConsole = true,
-    bool toFile = true
+    bool toFile = true,
+    bool immediatelyToFile = false
   }) {
     try {
       var s = '';
@@ -46,8 +52,15 @@ class Log {
       }
       s += data.toString();
       if (toConsole) print(s);
-      if (toFile) _writer.writeStringSync(s + '\n');
-      _logToArchive();
+      if (toFile) {
+        _buffer += (s + '\n');
+        if (immediatelyToFile || _buffer.length >= maxBufferSize) {
+          _writer.writeStringSync(_buffer);
+          _fileSize += _buffer.length;
+          _buffer = '';
+        }
+        _logToArchive();
+      }
     } catch(error) {
       if (onError == null) {
         throw Exception(error);
@@ -59,11 +72,12 @@ class Log {
 
 
   void _logToArchive() {
-    if (_file.lengthSync() >= maxLogFileSize) {
+    if (_fileSize >= maxLogFileSize) {
       var time = timeString(archiveFileTimeLength);
       _file.copySync(archiveFolder + time + ' - ' + fileName);
       _writer.setPositionSync(0);
       _writer.truncateSync(0);
+      _fileSize = 0;
     }
   }
 
